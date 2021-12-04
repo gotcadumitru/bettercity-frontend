97b41adb19784d4884d2e737f17074ba241f934d
gotca dumitru <dumitru.gotca@assist.ro>
2021-12-04 17:30:03 +0200

    fix bug

--- src/common/components/layout/Sidebar.jsx
+++ src/common/components/layout/Sidebar.jsx
@@ -6,23 +6,23 @@ import { AiOutlineBulb } from 'react-icons/ai';
 import { BiMapAlt } from 'react-icons/bi';
 import { useLocation } from 'react-router';
 import { Link } from 'react-router-dom';
-import { authToken } from '../../helpers/token';
+import { useSelector } from 'react-redux';
 const Sidebar = (props) => {
   const location = useLocation().pathname.split('/')[1];
+  const isAdmin = !useSelector((state) => state.profileInfo.user.data.role === 'admin');
   const navItems = [
-    { Icon: FiUsers, name: 'users', withAuth: true },
-    { Icon: GoIssueOpened, name: 'issues', withAuth: false },
-    { Icon: AiOutlineBulb, name: 'suggestions', withAuth: false },
-    { Icon: BiMapAlt, name: 'map', withAuth: false },
+    { Icon: FiUsers, name: 'users', forAdmin: true },
+    { Icon: GoIssueOpened, name: 'issues', forAdmin: false },
+    { Icon: AiOutlineBulb, name: 'suggestions', forAdmin: false },
+    { Icon: BiMapAlt, name: 'map', forAdmin: false },
   ];
 
-  const isUserAuthentificated = !!authToken.getToken();
   return (
     <div className="sidebar">
       <img className="sidebar__logo" src={logo} alt="BETTER CITY" />
       <hr className="sidebar__line" />
       {navItems
-        .filter(({ withAuth }) => !withAuth || isUserAuthentificated)
+        .filter(({ forAdmin }) => !forAdmin || isAdmin)
         .map(({ Icon, name }) => (
           <Link key={name} to={`/${name}`} className={`sidebar__item ${location === name ? 'sidebar__item--active' : ''}`}>
             <Icon className="sidebar__icon" /> {name}
