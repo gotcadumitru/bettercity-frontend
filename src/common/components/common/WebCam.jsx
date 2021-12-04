ed679832b744ff9ff63270859179fcf07c2950b9
gotca dumitru <dumitru.gotca@assist.ro>
2021-12-04 12:44:52 +0200

    add camera

--- src/common/components/common/WebCam.jsx
+++ src/common/components/common/WebCam.jsx
@@ -4,17 +4,6 @@ import Webcam from 'react-webcam';
 const WebcamCapture = ({ onChange, imgSrc, ...props }) => {
   const webcamRef = React.useRef(null);
 
-  const [deviceId, setDeviceId] = React.useState({});
-  const [devices, setDevices] = React.useState([]);
-
-  const handleDevices = React.useCallback(
-    (mediaDevices) => setDevices(mediaDevices.filter(({ kind }) => kind === 'videoinput')),
-    [setDevices]
-  );
-
-  React.useEffect(() => {
-    navigator.mediaDevices.enumerateDevices().then(handleDevices);
-  }, [handleDevices]);
   const capture = React.useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
     onChange(imageSrc);
@@ -22,14 +11,12 @@ const WebcamCapture = ({ onChange, imgSrc, ...props }) => {
 
   return (
     <>
-      {devices.map((device, key) => (
-        <div className={'web-camera'} key={key}>
-          <Webcam audio={false} ref={webcamRef} videoConstraints={{ deviceId: device.deviceId }} screenshotFormat="image/jpeg" />
-          <button className="button" onClick={capture}>
-            Capture photo
-          </button>
-        </div>
-      ))}
+      <div className={'web-camera'}>
+        <Webcam audio={false} ref={webcamRef} videoConstraints={{ facingMode: 'user' }} screenshotFormat="image/jpeg" />
+        <button className="button" onClick={capture}>
+          Capture photo
+        </button>
+      </div>
     </>
   );
 };
